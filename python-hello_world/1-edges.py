f550996caf108166a493e76f031e9831d46f9b7f
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-11-29 19:48:09 +0100

    ss

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word="Holberton"
+msg = "[Got]"
 word_first_3=(word[:3])
-print("First 3 letters: {}".format(word_first_3))
+print("First 3 letters: {}".format(msg, word_first_3))
 word_last_2= (word[-2:])
-print("Last 2 letters: {}".format(word_last_2))
+print("Last 2 letters: {}".format(msg, word_last_2))
 middle_word= (word[1:-1])
-print("Middle word: {}".format(middle_word))
-word="School"
+print("Middle word: {}".format(msg, middle_word))
