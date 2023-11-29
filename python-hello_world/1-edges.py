20cada4d466f0e0a6b59512fc96e3f379b64f533
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-11-29 21:49:23 +0100

    hh

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
-word="Holberton"
+word = "Holberton"
 
-word_first_3=(word[:3])
+word_first_3 = (word[:3])
+middle_word = (word[1:-1])
+word_last_2 = (word[-2:])
 print("First 3 letters: {}".format(word_first_3))
-word_last_2= (word[-2:])
 print("Last 2 letters: {}".format(word_last_2))
-middle_word= (word[1:-1])
 print("Middle word: {}".format(middle_word))
