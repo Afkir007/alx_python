0c88211dfb46b9a8e2e823a7bf7a4bf657d510da
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-11-30 16:51:39 +0100

    last commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,13 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}{}".format(i, j), end=", " if j < 9 else "\n")
+        print("{:d}{:d}".format(i, j), end="")
+        if i != 8 or j != 9:
+            print(", ", end="")
+
+print()
+
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+
+print ()
