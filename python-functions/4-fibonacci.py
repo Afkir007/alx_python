2c527c1e168193fe3d320ef642e8b6fe0353cd53
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-11-30 20:11:57 +0100

    fibo

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,11 +1,13 @@
 def fibonacci_sequence(n):
     if n <= 0:
         return []
-
-    fib_sequence = [0, 1]
-    while len(fib_sequence) < n:
-        next_num = fib_sequence[-1] + fib_sequence[-2]
-        fib_sequence.append(next_num)
-
-    return fib_sequence[:n]
-print(fibonacci_sequence(6))
+    elif n==1:
+        return [0]
+    elif n== 2:
+     return [0, 1]
+    else:
+       fib_sequence = [0, 1]
+       for _ in range(2, n):
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+    return fib_sequence
