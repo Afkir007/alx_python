319fe583c24f12d0deb5d8ac263b5826f50adaee
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-11-30 19:59:39 +0100

    comm

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    return fib_sequence[:n]
+print(fibonacci_sequence(6))
