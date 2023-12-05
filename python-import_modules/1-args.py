e79be8a233fbbca3547b816fecee488410e00733
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-12-05 13:56:44 +0100

    hh

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
 
 num_args = len(argv) - 1
 
-print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args), end="")
+print( "{} argument:".format("s" if num_args != 1 else "", num_args), end="")
 print(":" if num_args > 0 else ".")
 
 for i in range(1, num_args + 1):
