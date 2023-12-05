c34219b1c7de0202883f1ba62fd8506537b8cf4b
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-12-05 18:34:53 +0100

    lzs

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,3 +1,6 @@
-if __name__ == "__main__":
- def variable_load_2(a):
-  print(variable_load_2)
+variables = {}
+exec(open('variable_load_2.py').read(), variables)
+
+a_value = variables.get('a')
+
+print(a_value)
