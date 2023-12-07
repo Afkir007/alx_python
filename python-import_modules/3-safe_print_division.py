c95f6ddc2865b28d81c972a795e6568286458b5c
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-12-07 15:22:00 +0100

    safe

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,11 +4,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         print("Error: Division by zero is not allowed.")
         return None
-    except Exception as e:
-        print("An unexpected error occurred: {}".format(e))
-        return None
-    else:
-        print("Inside result: {}".format(result))
-        return result
     finally:
-        print("Finally block executed.")
+        print("Inside result: {}" .format(result))
+        return result
