0b50a1c52c868c0627adc202d8d9ca44efc40c14
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-12-07 12:31:03 +0100

    comit

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,5 @@
 if __name__ == "__main__":
   variables = {}
-exec(open('variable_load_2.py').read(), variables)
-
+  exec(open('variable_load_2.py').read(), variables)
 a_value = variables.get('a')
-
 print(a_value)
---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,14 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+        return None
+    except Exception as e:
+        print("An unexpected error occurred: {}".format(e))
+        return None
+    else:
+        print("Inside result: {}".format(result))
+        return result
+    finally:
+        print("Finally block executed.")
