20dcd0698bb91a218e2d775dbfcf614b874658d8
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-12-07 22:37:37 +0100

    map

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,2 +1,2 @@
 def best_score(a_dictionary):
-    return None if not a_dictionary else max(a_dictionary, key=a_dictionary.get)
+    return None if not a_dictionary else next(map(lambda k: k[1], sorted(map(lambda kv: (kv[1], kv[0]), a_dictionary.items()), reverse=True)))
