d38e285db6accd24d61b7fbc75459fe7c2723969
Mohieddine Afkir <afkirmouhiddine@gmail.com>
2023-12-13 18:39:23 +0100

    hh

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,31 @@
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): Private attribute representing the size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a square with the given size.
+
+    """
+
     def __init__(self, size):
-        self.__size = size 
+        """
+        Initializes a square with the given size.
+
+        Parameters:
+            size (int): The size of the square.
+
+        """
+        self.__size = size  # Private instance attribute
+
+    def get_size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+
+        """
+        return self.__size
